62da413050aa512328a736c6c5152388ad132d18
totoharyantoui <31500341+totoharyantoui@users.noreply.github.com>
2019-08-21 11:53:35 +0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,12 @@
+python3 Protein_Pred.py model_jst_1_262.csv output_4096_8.txt 4096
+python3 Protein_Pred.py model_jst_1_262.csv output_8192_8.txt 8194
+python3 Protein_Pred.py model_jst_1_262.csv output_16384_8.txt 16384
+python3 Protein_Pred.py model_jst_1_262.csv output_32768_8.txt 32768
+#python3.4 Protein_Pred.py model_jst_1_262.csv output_512_8.txt 512
+#python3.4 Protein_Pred.py model_jst_1_262.csv output_1024_.txt 1024
+#python3.4 Protein_Pred.py model_jst_1_262.csv output_2048_8.txt 2048
+
+
+
+
+
